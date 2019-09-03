ab72ee0e1b1a16e4ecf4f8044b70f62e031176d7
smv1999 <vaidhyanathan.sm@gmail.com>
2019-09-03 19:37:25 +0530

    ip

---
+++ adminhome.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "id10582486_smv1999";
+$password = "vaidhya@123";
+$dbname=    "id10582486_users";
+$aud=$_POST['audname'];
+$add=$_POST['address'];
+$seat=$_POST['seats'];
+$pr=$_POST['price'];
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql="INSERT INTO adminhome(audname,addr,seat,price) values ('$aud','$add','$seat','$pr')";
+$result=mysqli_query($conn,$sql);
+error_reporting(E_ALL ^ E_WARNING); 
+if($result)
+{
+header("Location:userlogin.html");
+echo "<script type='text/javascript'>alert('Successfully Inserted!');</script>";
+}
+else
+ echo "Error: " . $sql . "<br>" . mysqli_error($conn)
+?>
---
+++ adminlogin.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "id10582486_smv1999";
+$password = "vaidhya@123";
+$dbname=    "id10582486_users";
+$user=$_POST['uname'];
+$pswd=$_POST['psw'];
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$query = mysqli_query($conn,"SELECT * FROM adminlogin WHERE passwd='$pswd' AND username='$user'");
+$rows = mysqli_num_rows($query);
+if ($rows == 1) {
+header("location: adminhome.html"); 
+} else {
+$error = "Username or Password is invalid";
+echo "<script type='text/javascript'>alert('$error');</script>";
+}
+?>
